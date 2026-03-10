2d9af27fa06e151f70bae469055f1add72d5546e
vishakha-shete <vishakhashete2@gmail.com>
2026-03-10 19:32:08 +0530

    make some miner changes in signin-page

--- Frontend/src/pages/SingIn.jsx
+++ Frontend/src/pages/SingIn.jsx
@@ -16,7 +16,7 @@ const SingIn = () => {
                     "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('/background_banner.jpg')",
             }}
         >
-            <div className="max-w-[450px] w-full bg-black bg-opacity-75 rounded px-8 py-14 mx-auto mt-8">
+            <div className="max-w-112.5 w-full bg-black bg-opacity-75 rounded px-8 py-14 mx-auto mt-8">
                 <h1 className="text-3xl font-medium text-white mb-7">Sign In</h1>
                 <form className="flex flex-col space-y-4">
                     <input
@@ -24,14 +24,14 @@ const SingIn = () => {
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                         placeholder="username"
-                        className="w-full h-[50px] bg-[#333] text-white rouded px-5 text-base"
+                        className="w-full h-12.5 bg-[#333] text-white rouded px-5 text-base"
                     />
                     <input
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         placeholder="password"
-                        className="w-full h-[50px] bg-[#333] text-white rouded px-5 text-base"
+                        className="w-full h-12.5 bg-[#333] text-white rouded px-5 text-base"
                     />
                     <button
                         type="submit"
